26aeb19613df134e38fadc38d2e2915068ec189d
Royweru <royezi17@gmail.com>
2023-09-24 10:53:35 +0300

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   app/components/Navbar.tsx 	new file:   app/favicon.ico 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   app/page.tsx 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className=' border border-b-[2px]'>
+        <div>
+            Navbar is me
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <h1 className=' text-3xl text-red-300 font-bold'>
+       Carpool your ultimate car buyer and seller!
+      </h1>
+       
+    </main>
+  )
+}
