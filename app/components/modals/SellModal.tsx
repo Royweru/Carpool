34c419e0e644def6e11d2216dfc51233d982acb4
Royweru <royezi17@gmail.com>
2023-09-27 20:22:57 +0300

    modified:   app/components/modals/SellModal.tsx 	new file:   app/inputs/ImageUpload.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/components/modals/SellModal.tsx
+++ app/components/modals/SellModal.tsx
@@ -1,196 +1,193 @@
-'use client'
-import React,{useMemo, useState} from 'react'
-import Modal from './Modal'
-import useSellModal from '@/app/hooks/useSellModal'
-import { useForm,FieldErrors,FieldValues } from 'react-hook-form'
-import Heading from '../Heading'
-import Input from '@/app/inputs/Input'
+"use client";
+import React, { useMemo, useState } from "react";
+import Modal from "./Modal";
+import useSellModal from "@/app/hooks/useSellModal";
+import { useForm, FieldErrors, FieldValues } from "react-hook-form";
+import Heading from "../Heading";
+import Input from "@/app/inputs/Input";
 
-
-enum STEPS{
-    SELLER = 0,
-    INFO=1,
-    IMAGES=2,
-    PRICE=3
+enum STEPS {
+  SELLER = 0,
+  INFO = 1,
+  IMAGES = 2,
+  PRICE = 3,
 }
 const SellModal = () => {
-  const sellModal = useSellModal()
+  const sellModal = useSellModal();
 
-  const[step,setStep]= useState(STEPS.SELLER)
-   const onBack = ()=>{
-    setStep((value)=>value-1)
-   }
-   const onNext = ()=>{
-    setStep((value)=>value+1)
-   }
-  
- const  actionLabel = useMemo(()=>{
-      if (step == STEPS.PRICE){
-        return 'SUBMIT'
-      }
+  const [step, setStep] = useState(STEPS.SELLER);
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
 
-      return 'NEXT'
-   }, [step])
-
-const secondaryLabel = useMemo(()=>{
-    if (step == STEPS.SELLER){
-        return undefined
+  const actionLabel = useMemo(() => {
+    if (step == STEPS.PRICE) {
+      return "SUBMIT";
     }
-    return 'BACK'
-},[step])
-const {
-    register,   
+
+    return "NEXT";
+  }, [step]);
+
+  const secondaryLabel = useMemo(() => {
+    if (step == STEPS.SELLER) {
+      return undefined;
+    }
+    return "BACK";
+  }, [step]);
+  const {
+    register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
   } = useForm<FieldValues>({
     defaultValues: {
-      seller:"",
-      engineSize:"",
-      fuelType:"",
-      HP:"",
-      transmission:"",
-      torque:"",
-      aspiration:"",
-      acceleration:"",
-      make:"",
-      model:"",
-      image:[],
-      estimatedArrival:"",
-      mileage:1
+      seller: "",
+      engineSize: "",
+      fuelType: "",
+      HP: "",
+      transmission: "",
+      torque: "",
+      aspiration: "",
+      acceleration: "",
+      make: "",
+      model: "",
+      image: [],
+      estimatedArrival: "",
+      mileage: 1,
     },
   });
 
-let bodyContent;
+  let bodyContent;
 
-if(step === STEPS.SELLER){
+  if (step === STEPS.SELLER) {
+    bodyContent = (
+      <div className=" flex flex-col gap-8">
+        <Heading
+          title="Hey there our adored user, thank for choosing to trade on this platform"
+          subtitle="please make sure to fill the forms with the correct information and in no time you will bea assured to find a buyer"
+        />
 
-    bodyContent=(
-        <div className=' flex flex-col gap-8'>
-            <Heading
-              title='Hey there our adored user, thank for choosing to trade on this platform'
-              subtitle='please make sure to fill the forms with the correct information and in no time you will bea assured to find a buyer'
-              />
+        <hr />
+        <Heading title="Please write your name in full" />
+        <Input
+          id="seller"
+          label="Name"
+          register={register}
+          required
+          errors={errors}
+        />
+      </div>
+    );
+  }
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className=" flex flex-col gap-8">
+        <Heading
+          title=" Provide us with the information of your car"
+          subtitle="make sure you fill out the whole form"
+        />
+        <hr />
+        <Input
+          id="YOM"
+          label="Year of manufacture"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="estimatedArrival"
+          label="In which time will it be availbale"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="drive"
+          type="text"
+          label="Drive"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="mileage"
+          label="Mileage"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="engineSize"
+          label="Engine size"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="fuelType"
+          label="Fuel type"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="HP"
+          label="Horse Power"
+          register={register}
+          required
+          errors={errors}
+        />
 
-              <hr />
-            <Heading title='Please write your name in full'/>
-            <Input
-             id='seller'
-             label='Name'
-             register={register}
-             required
-             errors={errors}
-             />
-           
-        </div>
+        <Input
+          id="transmission"
+          label="Transmission"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="aspiration"
+          label="Aspiration"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="torque"
+          label="Torque"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="make"
+          label="The make e.g Toyota"
+          register={register}
+          required
+          errors={errors}
+        />
+        <Input
+          id="model"
+          label="The model e.g vitz"
+          register={register}
+          required
+          errors={errors}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className=" flex flex-col gap-8">
+         <Heading title="Image uploads" subtitle="upload max of 3 images" />
+         <ImageUpload />
+      </div>
     )
-}
-if(step===STEPS.INFO){
-    bodyContent=(
-       <div className=' flex flex-col gap-8'>
-          <Heading title=' Provide us with the information of your car' subtitle='make sure you fill out the whole form'/>
-          <hr />
-          <Input
-             id='YOM'
-             type='text'
-             label='Year of manufacture'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='estimatedArrival'
-             type='text'
-             label='In which time will it be availbale'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='drive'
-             type='text'
-             label='Drive'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='mileage'
-             type='nummber'
-             label='Mileage'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='engineSize'
-             type='text'
-             label='Engine size'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='fuelType'
-             label='Fuel type'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='HP'
-             type='text'
-             label='Horse Power'
-             register={register}
-             required
-             errors={errors}
-             />
-           
-               <Input
-             id='transmission'
-             type='text'
-             label='Transmission'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='aspiration'
-             
-             label='Aspiration'
-             register={register}
-             required
-             errors={errors}
-             />
-             <Input
-             id='torque'
-             
-             label='Torque'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='make'
-             type='text'
-             label='The make e.g Toyota'
-             register={register}
-             required
-             errors={errors}
-             />
-               <Input
-             id='model'
-             type='text'
-             label='The model e.g vitz'
-             register={register}
-             required
-             errors={errors}
-             />
-             
-       </div>
-    )
-}
-
+  }
 
   return (
     <Modal
@@ -199,11 +196,11 @@ if(step===STEPS.INFO){
       onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryLabel={secondaryLabel}
-      secondaryAction={step ==STEPS.SELLER ?undefined:onBack}
-      title='Find a buyer for your car today!'
+      secondaryAction={step == STEPS.SELLER ? undefined : onBack}
+      title="Find a buyer for your car today!"
       body={bodyContent}
-      />
-  )
-}
+    />
+  );
+};
 
-export default SellModal
+export default SellModal;
---
+++ app/inputs/ImageUpload.tsx
@@ -0,0 +1,52 @@
+import React, { useCallback } from 'react'
+import { CldUploadWidget } from 'next-cloudinary'
+import{TbPhotoPlus} from 'react-icons/tb'
+
+interface ImageUploadProps{
+    onChange:(value:string)=>void,
+    value:string
+}
+const ImageUpload:React.FC<ImageUploadProps> = ({onChange, value}) => {
+    const submitImage  = useCallback((result:any)=>{
+        onChange(result.info.secure_url) 
+    },[onChange])
+  return (
+    <CldUploadWidget
+      onUpload={submitImage}
+      uploadPreset="ksr4nuvh"
+      options={{
+        maxFiles: 1,
+      }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className=" relative 
+            cursor-pointer 
+            hover:opacity-70
+             transition
+              border-dashed 
+              border-2 p-20
+               border-neutral-300 
+               flex 
+               flex-col 
+               justify-center
+                items-center 
+                gap-4
+                 text-neutral-600"
+          >
+            <TbPhotoPlus/>
+            <div className=" font-semibold text-lg text-red-500">
+                Cick to upload
+            </div>
+           
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+  
+    }
+
+export default ImageUpload
